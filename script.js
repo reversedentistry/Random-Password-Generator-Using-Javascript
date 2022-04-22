b1766d34231fb34f2e886602fc7ba7d76a745a44
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2022-04-22 15:32:10 -0700

    Added parameters to number prompt

--- script.js
+++ script.js
@@ -7,39 +7,52 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
 
-  var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  function generatePassword() {
+    var passLength = Number(window.prompt("How many characters would you like your password to be? Enter a number between 8 and 128.")); 
+      if (passLength < 8) {
+        alert( "Password must be at least 8 characters");
+        var passLength; 
+      }
+      if (passLength > 128) {
+        alert ("Password cannot be greater than 128 characters"); 
+        var passLength; 
+      }
 
-  var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];   
-
-  var special = ["$", "#", "!", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", "<", ">", "~", "?", "@", "\", "^", "|"]; 
-
-  var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]; 
-
-  var passLength = window.prompt("How many characters would you like your password to be? Enter a number between 8 and 128.")
-
-  var lowerPrompt = window.confirm("Would you like lowercase letters to be included?");
+    var lowerPrompt = window.confirm("Would you like lowercase letters to be included?");
   // If Y choose random letter from lowerCase and append/add?
   // lowerPrompt = lowerPrompt.toUpperCase(); 
 
-  var upperPrompt = window.confirm("Would you like uppercase letters to be included?");
+    var upperPrompt = window.confirm("Would you like uppercase letters to be included?");
   // If Y choose random from upperCase and append
   // upperPrompt = upperPrompt.toUpperCase(); 
 
-  var numPrompt= window.confirm("Would you like numbers to be included?");
+    var numPrompt= window.confirm("Would you like numbers to be included?");
   // If Y choose random from numbers and append
   // numPrompt = numPrompt.toUpperCase(); 
 
-  var specPrompt = window.confirm("Would you like special characters to be included?");
+    var specPrompt = window.confirm("Would you like special characters to be included?");
     //If Y choose random from special and append 
   // specPrompt = specPrompt.toUpperCase(); 
-  // After these first four are selected, concat all selected arrays to select random characters for rest of password 
+    
+    var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-  function superArray() {
-    if (lowerPrompt === "Y"){
-      
-    }
+    var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];   
+
+    var special = ["$", "#", "!", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", "<", ">", "~", "?", "@", "\", "^", "|"]; 
+
+    var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]; 
+
+    
+  // After these first four are selected, aggregate all selected arrays to select random characters for rest of password 
+
+    var superArray = [];
+
+      if (lowerPrompt){
+      password.append(Math.floor(Math.random() * lowerCase.length)); 
+
+  
   }
-
+}
    
 
   
