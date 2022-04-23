be0441f8e7376f0b0654277bff9091375e33974c
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2022-04-22 23:12:28 -0700

    Added statements to aggregate arrays based on user choices

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m"
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];   
 var special = ["$", "#", "!", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", "<", ">", "~", "?", "@", '^', '|']; 
 var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]; 
-var superArray = [];
+
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
@@ -16,36 +16,52 @@ function writePassword() {
 
 function generatePassword() {
     var password = ""; 
-   
+    var superArray = [];
+
     var passLength = Number(window.prompt("How many characters would you like your password to be? Enter a number between 8 and 128.")); 
       if (passLength < 8) {
         alert( "Password must be at least 8 characters. Please try again.");
-        var passLength; 
+        return;
       }
       if (passLength > 128) {
         alert ("Password cannot be greater than 128 characters. Please try again."); 
-        var passLength; 
+        return;  
+      }
+      if (!passLength) {
+        return; 
       }
 
     var lowerPrompt = window.confirm("Would you like lowercase letters to be included?");
   // If Y choose random letter from lowerCase and append/add
       if (lowerPrompt) {
-        
-      }
+        superArray = superArray.concat(lowerCase);       }
   
     var upperPrompt = window.confirm("Would you like uppercase letters to be included?");
   // If Y choose random from upperCase and append
+      if (upperPrompt) {
+        superArray = superArray.concat(upperCase); 
+      }
   
     var numPrompt= window.confirm("Would you like numbers to be included?");
   // If Y choose random from numbers and append
+      if (numPrompt) {
+        superArray = superArray.concat(numbers); 
+      }
   
     var specPrompt = window.confirm("Would you like special characters to be included?");
     //If Y choose random from special and append 
+      if (specPrompt) {
+        superArray = superArray.concat(special); 
+      }
+
+    console.log(superArray); 
   
     
   // After these first four are selected, aggregate all selected arrays to select random characters for rest of password 
 
+  for (i = 0 ; i < passLength; i++) {
     
+  }
     
     /*var passNum = function () {
       if (numPrompt) {
@@ -55,7 +71,7 @@ function generatePassword() {
    
        
     Math.floor(Math.random() * [array].length)*/
-     for (var i = 0; i < passLength; i++) 
+     /*for (var i = 0; i < passLength; i++) 
      {
        if (lowerPrompt) {
 
@@ -73,7 +89,7 @@ function generatePassword() {
          passwordText.append(number[Math.floor(Math.random() * numbers.length)]); 
       }
     }
-    console.log(passwordText);   
+    console.log(passwordText); */ 
        
   }
   
